17702c44a473a33f1a9e8041fb1b603c883949d5
simon0302010 <simon0302010@gmail.com>
2025-08-09 01:11:07 -0400

    simplify code; add code to change the appearance of the flashright based on whether it's turned on or off

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import utime
 
 class App(badge.BaseApp):
     def __init__(self) -> None:
+        self.led_on = 0
         pass
 
     def on_open(self) -> None:
-        badge.utils.set_led(1)
-        self.draw_flashlight()        
+        self.turn_on()
 
-    def draw_flashlight(self, ) -> None:
+    def draw_flashlight(self) -> None:
         badge.display.fill(1)
         flashlight_lines = [
             # flashlight icon
@@ -23,15 +23,18 @@ class App(badge.BaseApp):
             ((120, 45), (120, 60)),
             ((120, 60), (80, 60)),
             
-            ((100, 45), (90, 25)),
-            ((100, 45), (100, 25)),
-            ((100, 45), (110, 25)),
-            
             ((90, 80), (110, 80)),
             ((90, 95), (110, 95)),
             ((90, 110), (110, 110)),
             ((90, 125), (110, 125))
         ]
+
+        if self.led_on:
+            flashlight_lines.extend([
+                ((100, 45), (90, 25)),
+                ((100, 45), (100, 25)),
+                ((100, 45), (110, 25))
+            ])
 
         # draw icon
         for start_point, end_point in flashlight_lines:
@@ -40,6 +43,16 @@ class App(badge.BaseApp):
             badge.display.line(x1, y1, x2, y2, 0)
         
         badge.display.show()
+
+    def turn_on(self) -> None:
+        badge.utils.set_led(1)
+        self.led_on = 1
+        self.draw_flashlight()
+
+    def turn_off(self) -> None:
+        badge.utils.set_led(0)
+        self.led_on = 0
+        self.draw_flashlight()
 
     def loop(self) -> None:
         utime.sleep(1)
